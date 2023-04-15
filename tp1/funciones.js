ec8327c0a3a5225e549abfe46ba4efcc0c8069c5
Ignacio Piter <129554368+IgnacioPiter@users.noreply.github.com>
2023-04-14 22:14:23 -0300

    Soy ignacio piter tp1

---
+++ tp1/funciones.js
@@ -0,0 +1,8 @@
+function mifuncion() {
+    var nombre = document.getElementsByName("Nombre")[0].value;
+    var apellido = document.getElementsByName("Apellido")[0].value;
+    var edad = document.getElementsByName("Edad")[0].value;
+  
+    alert("Nombre: " + nombre + "\nApellido: " + apellido + "\nEdad: " + edad);
+  }
+  
