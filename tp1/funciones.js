de149b7a36d1a999c0bd8b225e803cea941adbeb
Ignacio Piter <129554368+IgnacioPiter@users.noreply.github.com>
2024-03-21 19:51:47 -0300

    Delete tp1 directory

--- tp1/funciones.js
+++
@@ -1,8 +0,0 @@
-function mifuncion() {
-    var nombre = document.getElementsByName("Nombre")[0].value;
-    var apellido = document.getElementsByName("Apellido")[0].value;
-    var edad = document.getElementsByName("Edad")[0].value;
-  
-    alert("Nombre: " + nombre + "\nApellido: " + apellido + "\nEdad: " + edad);
-  }
-  
